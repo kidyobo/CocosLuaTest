7e0ea7ebc765c4f9f8f567cb0ca774ce3163ef74
Rookie <1176814811@qq.com>
2020-07-14 12:04:20 +0800

    添加开箱子逻辑

--- luatest/src/app/scenes/MainScene.lua
+++ luatest/src/app/scenes/MainScene.lua
@@ -4,9 +4,69 @@ local MainScene = class("MainScene", function()
 end)
 
 function MainScene:ctor()
-    display.newTTFLabel({text = "Hello, World", size = 64})
-        :align(display.CENTER, display.cx, display.cy)
-        :addTo(self)
+    local layer = display.newColorLayer(cc.c4b(255,255,255,255))
+    local cardNode = display.newNode()
+    self:addChild(layer)
+    self:addChild(cardNode)
+    local chest_close = display.newSprite("chest_close.png")
+    local chest_open = display.newSprite("chest_open.png")
+    
+    local cards = {}
+    for i=1,20 do
+        cards[i] = i
+    end
+    
+    chest_open:setVisible(false)
+    layer:addChild(chest_close)
+    layer:addChild(chest_open)
+    chest_open:setPosition(display.width/2,display.height*0.75)
+    chest_close:setPosition(display.width/2,display.height*0.75)
+    self:addNodeEventListener(cc.NODE_TOUCH_EVENT, function(event) 
+        chest_open:setVisible(true)
+        chest_close:setVisible(false)
+        local randSend = self:randArray(cards)
+        local randArrayResult = {}
+        local rand = math.random
+        math.randomseed(os.time())
+        chest_close:setPosition(display.width/2,display.height*0.75)
+
+        local randNumber = rand(5,7)
+        cardNode:removeAllChildren()
+        local startPos = cc.p(display.width/4,display.height/2-100)
+        for i=1,randNumber do
+            randArrayResult[i] = randSend[i]
+            print("rands:"..randArrayResult[i])
+            local path = "card/card_"..randArrayResult[i]..".png"
+            local card = display.newSprite(path)
+            cardNode:addChild(card)
+            local posx = 0
+            local posy = 0
+            if i<=4 then
+                posx = startPos.x+300*(i-1)
+                posy = startPos.y
+            else
+                posx = startPos.x+300*(i-5)
+                posy = startPos.y-300
+            end
+            card:setPosition(posx,posy)
+        end
+       
+    end)
+    self:setTouchEnabled(true)
+
+end
+--打乱数组
+function MainScene:randArray(arr)
+    local tmp, index
+    for i=1, #arr-1 do
+        index = math.random(i, #arr)
+        if i ~= index then
+            tmp = arr[index]
+            arr[index] = arr[i]
+            arr[i] = tmp
+        end
+    end
+    return arr
 end
 
 function MainScene:onEnter()
@@ -14,5 +74,6 @@ end
 
 function MainScene:onExit()
 end
+
 
 return MainScene
--- luatest/src/config.lua
+++ luatest/src/config.lua
@@ -3,14 +3,14 @@
 DEBUG = 1
 
 -- display FPS stats on screen
-DEBUG_FPS = true
+DEBUG_FPS = false
 
 -- dump memory info every 10 seconds
 DEBUG_MEM = false
 
 -- design resolution
-CONFIG_SCREEN_WIDTH  = 960
-CONFIG_SCREEN_HEIGHT = 640
+CONFIG_SCREEN_WIDTH  = 1920
+CONFIG_SCREEN_HEIGHT = 1080
 
 -- auto scale mode
 CONFIG_SCREEN_AUTOSCALE = "FIXED_HEIGHT"
